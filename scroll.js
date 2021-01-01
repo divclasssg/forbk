b13f1abbcf2ab217174a35c8f37d79faddc3a74e
divclasssg <49296945+divclasssg@users.noreply.github.com>
2021-01-01 22:36:30 +0900

    Add files via upload

--- scroll.js
+++ scroll.js
@@ -14,7 +14,7 @@
   })
   t0.from('.img-groom', {opacity: 0}, 0)
   t0.from('.greeting.groom', {color: '#1d1d1f', duration: 0.1}, 0)
-  t0.from('.greeting.groom .greeting-text-1', {fontSize: 0}, 0)
+  t0.from('.greeting.groom .greeting-text-1', {transform: 'scale(0)', height:0}, 0)
   t0.to('.greeting.groom', {y: -innerHeight, opacity: 0})
 
   // t1
@@ -31,7 +31,7 @@
   })
   t1.from('.img-bride', {opacity: 0}, 0)
   t1.from('.greeting.bride', {color: '#1d1d1f', duration: 0.1}, 0)
-  t1.from('.greeting.bride .greeting-text-1', {fontSize: 0}, 0)
+  t1.from('.greeting.bride .greeting-text-1', {transform: 'scale(0)', height:0}, 0)
   t1.to('.greeting.bride', {y: -innerHeight, opacity: 0})
 
   //t2
@@ -46,21 +46,23 @@
       // markers:  true
     }
   })
-  t2.to('.t2 .img-groom', {x: -67.9 + '%', delay: 0.19}, 0)
-  t2.to('.t2 .img-bride', {x: 32 + '%'}, 0)
+  // t2.to('.t2 .img-groom', {x: -67.9 + '%', delay: 0.19}, 0)
+  // t2.to('.t2 .img-bride', {x: 32 + '%'}, 0)
+  t2.from('.t2 .img-together', {opacity: 0})
   t2.fromTo('.we-get-married', {opacity: 0}, {opacity: 1, delay: 0.5}, 1)
   t2.to('.we-get-married', {transform: 'scale(10)', opacity: 0, delay: 1, duration: 5}, 1)
   // t2.to('.t2 .flex-center', {backgroundColor: '#1d1d1f', delay: 0.45}, 1)
   // t2.to('.we-get-married', {opacity: 0, delay: 0.15}, 1)
-  t2.to('.img-together-wrapper', {backgroundColor: '#1d1d1f'}, 2)
-  t2.to('.img-together-wrapper img', {opacity: 0}, 2)
+  // t2.to('.img-together-wrapper', {backgroundColor: '#1d1d1f'}, 2)
+  // t2.to('.img-together-wrapper img', {opacity: 0}, 2)
+  t2.to('.t2 .img-together', {opacity: 0})
 
   //t3
   let t3 = gsap.timeline({
     scrollTrigger: {
       trigger: '.t3',
       start: 'center center',
-      end: () => '+=' + innerHeight * 5,
+      end: () => '+=' + innerHeight * 6,
       // end: 'bottom +=100%',
       scrub: 1,
       pin: true,
@@ -72,7 +74,7 @@
   t3.fromTo('.text-when', {opacity: 0}, {opacity: 1, y: -10})
   t3.from('.when-answer', {opacity: 0})
   t3.to('.text-so, .text-when', {opacity: 0})
-  t3.to('.when-answer, .when-answer p', {fontSize: '2rem', display: 'flex', y: -80})
+  t3.to('.when-answer, .when-answer p', {fontSize: '2rem', display: 'flex', y: -80, duration: 3})
   t3.fromTo('.text-where', {opacity: 0}, {opacity: 1, y: -10})
   t3.fromTo('.text-where-answer', {opacity: 0}, {opacity: 1, y: -10})
   t3.from('.dark-background', {x: -50 + '%', y: -50 + '%', transform: 'scale(0)'})
@@ -96,17 +98,17 @@
     scrollTrigger: {
       trigger: '.t5',
       start: 'center center',
-      end: () => '+=' + innerHeight * 4,
+      end: () => '+=' + innerHeight * 2.5,
       scrub: 1,
       pin: true,
       anticipatePin: 1
       // markers: true
     }
   })
-  t5.from('.text-well', {opacity: 0})
-  t5.from('.text-like-this', {opacity: 0}, 1)
-  t5.from('.img-together2', {opacity: 0}, 1)
-  t5.to('.img-together2', {width: '120%', left: 0, bottom: 0}, 2)
+  // t5.from('.text-well', {opacity: 0})
+  t5.from('.text-like-this', {opacity: 0}, 0)
+  t5.from('.img-together2', {opacity: 0}, 0)
+  t5.to('.img-together2', {width: '120%', left: 0, bottom: 0, duration: 2}, 2)
   t5.to('.text-well, .text-like-this', {opacity: 0}, 2)
 
 })();
